0aacd9643c88a98713632b6a807b071b13cc3449
ArtemijsRadionovs <124582137+ArtemijsRadionovs@users.noreply.github.com>
2023-03-19 16:30:25 +0200

    Final

--- build_heap.py
+++ build_heap.py
@@ -22,7 +22,7 @@ def build_heap(data):
     # try to achieve  O(n) and not O(n2)
 
     n = len(data)
-    for i in range(n):
+    for i in range(n // 2, -1, -1):
         sift_down(data, i, swaps)
     return swaps
 
