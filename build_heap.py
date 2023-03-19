1470559d0b7bba4b83eb0fb39b4d22debc4521a3
ArtemijsRadionovs <124582137+ArtemijsRadionovs@users.noreply.github.com>
2023-03-19 03:25:04 +0200

    Third

--- build_heap.py
+++ build_heap.py
@@ -1,5 +1,18 @@
 # python3
 
+def sift_down(data, i, swaps):
+        n = len(data)
+        min_index = i
+        l = 2 * i + 1
+        if l < n and data[l] < data[min_index]:
+            min_index = l
+        r = 2 * i + 2
+        if r < n and data[r] < data[min_index]:
+            min_index = r
+        if i != min_index:
+            data[i], data[min_index] = data[min_index], data[i]
+            swaps.append((i, min_index))
+            sift_down(data, min_index, swaps)
 
 def build_heap(data):
     swaps = []
@@ -9,7 +22,6 @@ def build_heap(data):
     for i in range(n // 2, -1, -1):
         sift_down(data, i, swaps)
     return swaps
-
 
 def main():
     
@@ -42,19 +54,7 @@ def main():
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
     
-    def sift_down(data, i, swaps):
-        n = len(data)
-        min_index = i
-        l = 2 * i + 1
-        if l < n and data[l] < data[min_index]:
-            min_index = l
-        r = 2 * i + 2
-        if r < n and data[r] < data[min_index]:
-            min_index = r
-        if i != min_index:
-            data[i], data[min_index] = data[min_index], data[i]
-            swaps.append((i, min_index))
-            sift_down(data, min_index, swaps)
+    
 
     # output all swaps
     print(len(swaps))
