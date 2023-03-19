109c13ae075174ddb847c351a262d9790382933d
ArtemijsRadionovs <124582137+ArtemijsRadionovs@users.noreply.github.com>
2023-03-19 03:10:00 +0200

    First

--- build_heap.py
+++ build_heap.py
@@ -5,8 +5,9 @@ def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    n = len(data)
+    for i in range(n // 2, -1, -1):
+        sift_down(data, i, swaps)
     return swaps
 
 
@@ -18,9 +19,19 @@ def main():
 
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
+    check_for_I = input().replace('\r','')
+    if check_for_I == "I":
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif check_for_I == "F":
+        file_name = input().replace('\r','')
+        try:
+            with open('./test/' + file_name, 'r') as f:
+                n = int(f.readline())
+                data = list(map(int, f.readline().split()))
+            except FileNotFoundError:
+                print("File not found!")
+                return
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
@@ -30,7 +41,20 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
+    
+    def sift_down(data, i, swaps):
+    n = len(data)
+    min_index = i
+    l = 2 * i + 1
+    if l < n and data[l] < data[min_index]:
+        min_index = l
+    r = 2 * i + 2
+    if r < n and data[r] < data[min_index]:
+        min_index = r
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        sift_down(data, min_index, swaps)
 
     # output all swaps
     print(len(swaps))
